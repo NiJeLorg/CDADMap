aca94d4c02f6684aab70718986bb5f300b92b62d
JD Godchaux <jd@nijel.org>
2014-12-22 18:52:24 -0500

    continued work on filters, UI complete

--- cdad/cdadmap/static/cdadmap/js/CDADMap.js
+++ cdad/cdadmap/static/cdadmap/js/CDADMap.js
@@ -302,7 +302,26 @@ CDADMap.onEachFeatureFor_LOCATIONS = function(feature, layer){
 		
 		// update sidebar content based on click
 		$( "#popout-info-content" ).html("<div class='info-title-bar text-capitalize'>" + feature.properties.Organization_Name + "</div><div class='info-content-titles'>OFFICE ADDRESS</div><p class='info-content'>" + feature.properties.Address + " " + feature.properties.Address2 + "<br>" + feature.properties.City + ", " + feature.properties.State + " " + feature.properties.ZipCode + "</p><div class='info-content-titles'>DESCRIPTION</div><p class='info-content'>" + feature.properties.Organization_Description + "</p><div class='info-content-titles'>PRIMARY FOCUS AREAS</div><p class='info-content'>" + activityList + "</p><div class='info-content-titles'>SERVICES</div><p class='info-content'>" + activities_ServicesList + "</p><div class='info-content-titles'>CONTACT</div><p class='info-content'>" + feature.properties.Tel + "<br><a href='mailto:" + feature.properties.Email + "'>" + feature.properties.Email + "</a></p><div class='info-content-titles'>WEBSITE</div><p class='info-content'><a href='" + url + "'>" + printurl + "</a></p><br><button type='button' class='btn btn-default btn-block' data-toggle='modal' data-target='#orgModal' data-local='#orgCarousel' id='slideTo" + feature.properties.idlocation + "'><span class='pull-left'>VIEW FULL PROFILE</span><span class='glyphicon glyphicon-fullscreen pull-right' aria-hidden='true'></span></button>");
-		
+				
+		var slideToIdlocation = '#slideTo' + parseInt(feature.properties.idlocation);
+		var modalItemId = '#modalItem' + parseInt(feature.properties.idlocation);
+		$(slideToIdlocation).click(function() {
+			// rotate carousel
+			$( "#orgCarousel" ).carousel(parseInt(feature.properties.counterId));
+			
+			setTimeout(function() {
+				
+				maps[feature.properties.idlocation].map.invalidateSize();
+				
+				// update modal title
+				if ($( modalItemId ).hasClass('active')) {
+					$( "#titleOrgName" ).html(feature.properties.Organization_Name);						
+				}
+				
+			}, 200);
+			
+		});
+				
 		// populate banner				
 		$( "#banner-text" ).html("INFO");
 
@@ -341,11 +360,6 @@ CDADMap.onEachFeatureFor_LOCATIONS = function(feature, layer){
 		// set up modal for having this org selected
 		$( "#titleOrgName" ).html(feature.properties.Organization_Name);
 		
-		/* move carousel to the correct slide
-		var idlocation = parseInt(feature.properties.idlocation);
-		console.log(idlocation);
-		$( ".carousel" ).carousel(idlocation);
-		*/
 		
 
 	});
--- cdad/cdadmap/views.py
+++ cdad/cdadmap/views.py
@@ -16,9 +16,80 @@ def index(request):
     kwargs['KeepPrivate__exact'] = 'false'
     
     #get locations
-    locations = LocationPanel.objects.filter(**kwargs)            
+    locations = LocationPanel.objects.filter(**kwargs)
     
-    context_dict = {'locations': locations}
+        
+    # get lists of options for filters
+    Organization_Description_Choices_Array = []
+    Organization_Description_Choices_List = SurveyPanel.objects.values_list('Organization_Description', flat=True)
+    for choice in Organization_Description_Choices_List:
+        choice_string = str(choice)
+        split_choices = choice_string.split(', ');
+        Organization_Description_Choices_Array.extend(split_choices)   
+        
+    Organization_Description_Choices = list(set(Organization_Description_Choices_Array))
+    Organization_Description_Choices.sort()
+
+
+    Service_Area_Choices_Array = []
+    Service_Area_Choices_List = SurveyPanel.objects.values_list('Service_Area_Description', flat=True)    
+    for choice in Service_Area_Choices_List:
+        choice_string = str(choice)
+        split_choices = choice_string.split(',');
+        Service_Area_Choices_Array.extend(split_choices)   
+
+    Service_Area_Choices = list(set(Service_Area_Choices_Array))
+    Service_Area_Choices.sort()
+
+
+    organization_structured_Choices_Array = []
+    organization_structured_Choices_List = SurveyPanel.objects.values_list('organization_structured', flat=True)
+    for choice in organization_structured_Choices_List:
+        choice_string = str(choice)
+        choice_string_replace = choice_string.replace(', ', '| ')
+        split_choices = choice_string_replace.split(',')
+        organization_structured_Choices_Array.extend(s.replace('| ', ', ') for s in split_choices)   
+
+    organization_structured_Choices = list(set(organization_structured_Choices_Array))        
+    organization_structured_Choices.sort()
+
+    Activities_Services_Choices_Array = []
+    Activities_Services_Choices_List = SurveyPanel.objects.values_list('Activities_Services', flat=True)    
+    for choice in Activities_Services_Choices_List:
+        choice_string = str(choice)
+        choice_string_replace = choice_string.replace(', ', '| ')
+        split_choices = choice_string_replace.split(',')
+        Activities_Services_Choices_Array.extend(s.replace('| ', ', ') for s in split_choices)   
+
+    Activities_Services_Choices = list(set(Activities_Services_Choices_Array))        
+    Activities_Services_Choices.sort()
+
+    Service_Population_Choices_Array = []
+    Service_Population_Choices_List = SurveyPanel.objects.values_list('Service_Population', flat=True)    
+    for choice in Service_Population_Choices_List:
+        choice_string = str(choice)
+        choice_string_replace = choice_string.replace(', ', '| ')
+        split_choices = choice_string_replace.split(',')
+        Service_Population_Choices_Array.extend(s.replace('| ', ', ') for s in split_choices)   
+
+    Service_Population_Choices = list(set(Service_Population_Choices_Array))        
+    Service_Population_Choices.sort()
+
+    Languages_Choices_Array = []
+    Languages_Choices_List = SurveyPanel.objects.values_list('Languages', flat=True)    
+    for choice in Languages_Choices_List:
+        choice_string = str(choice)
+        choice_string_replace = choice_string.replace(', ', '| ')
+        split_choices = choice_string_replace.split(',')
+        Languages_Choices_Array.extend(s.replace('| ', ', ') for s in split_choices)   
+
+    Languages_Choices = list(set(Languages_Choices_Array))        
+    Languages_Choices.sort()
+
+
+
+
+    context_dict = {'locations': locations, "Organization_Description_Choices": Organization_Description_Choices, "Service_Area_Choices": Service_Area_Choices, "organization_structured_Choices": organization_structured_Choices, "Activities_Services_Choices": Activities_Services_Choices, "Service_Population_Choices": Service_Population_Choices, "Languages_Choices": Languages_Choices }
     return render_to_response('cdadmap/index.html', context_dict, context)
     
 
