9a8a62d0b26a3e7d50363a49615a21ae40d4cd18
JD Godchaux <jd@nijel.org>
2015-08-12 19:23:48 -0400

    update

--- cdad/cdadmap/migrations/0004_auto_20141216_1541.py
+++ cdad/cdadmap/migrations/0004_auto_20141216_1541.py
@@ -11,22 +11,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterField(
-            model_name='contactpanel',
-            name='Organization_Name',
-            field=models.ForeignKey(to='cdadmap.SurveyPanel', to_field=b'Organization_Name', null=True),
-            preserve_default=True,
-        ),
-        migrations.AlterField(
-            model_name='locationpanel',
-            name='Organization_Name',
-            field=models.ForeignKey(to='cdadmap.SurveyPanel', to_field=b'Organization_Name', null=True),
-            preserve_default=True,
-        ),
-        migrations.AlterField(
-            model_name='meetingpanel',
-            name='Organization_Name',
-            field=models.ForeignKey(to='cdadmap.SurveyPanel', to_field=b'Organization_Name', null=True),
-            preserve_default=True,
-        ),
+
     ]
