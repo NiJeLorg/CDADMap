5f277cc0f4e881b001578047e0f15b5dbbcc4694
JD Godchaux <jd@nijel.org>
2015-08-12 17:17:50 -0400

    update

--- cdad/cdadmap/migrations/0033_auto_20150812_1704.py
+++ cdad/cdadmap/migrations/0033_auto_20150812_1704.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='surveypanel',
             name='Organization_Name',
-            field=models.CharField(default=b'', unique=True, max_length=2000),
+            field=models.CharField(default=b'', unique=True, max_length=255),
             preserve_default=True,
         ),
     ]
