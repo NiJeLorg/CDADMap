cb9808f8483ce6d2d39153381224e90f8665f14d
JD Godchaux <jd@nijel.org>
2015-11-09 16:29:18 -0500

    testing

--- cdad/cdadmap/static/cdadmap/js/CDADModalMap.js
+++ cdad/cdadmap/static/cdadmap/js/CDADModalMap.js
@@ -60,6 +60,7 @@ function CDADModalMap(mapid, lat, lon, zoom, surveyid, type, numid) {
 		//var southWest = L.latLng(42.257746, -83.295593);
 		//var northEast = L.latLng(42.489061, -82.853394);
 		//bounds[numid] = L.latLngBounds(southWest, northEast);
+		console.log(zoom);
 
 		bounds[numid] = this.map.getBounds();
 
